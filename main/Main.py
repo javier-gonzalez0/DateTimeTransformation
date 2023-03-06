936c5ad15b1dd7e8f04c0c2d7e2fb3be203eb91e
Javier Gonzalez <61213597+javier-gonzalez0@users.noreply.github.com>
2023-03-05 19:31:12 -0600

    Create Main.py
    Initial Commit

---
+++ main/Main.py
@@ -0,0 +1,23 @@
+#################################
+#   Javier Gonzalez             #
+#   UTSA ECE                    #
+#   CS 5103                     #
+#################################
+
+# Useful Material below
+# https://www.datacamp.com/tutorial/k-nearest-neighbor-classification-scikit-learn
+# https://scikit-learn.org/stable/modules/neighbors.html#neighborhood-components-analysis
+# http://archive.ics.uci.edu/ml/datasets/Iris
+
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn import metrics
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+from matplotlib.colors import ListedColormap
+from sklearn import neighbors, datasets
+from sklearn.inspection import DecisionBoundaryDisplay
+
+
+if __name__ == '__main__':
+    print("This is the template for the project work")
